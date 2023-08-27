7150b9d7912eae2369b1bfd6b5a952c7478127f1
Ryan Biondo <biondoryan.dev@gmail.com>
2023-08-27 08:36:49 -0600

    login - working with reducers

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import TodoForm from './react-query/TodoForm';
 import TodoList from './react-query/TodoList';
 import Counter from './state-management/Counter';
 import TaskList from './state-management/TaskList';
+import LoginStatus from './state-management/LoginStatus';
 
 function App() {
   return (
     <>
-      <TaskList />
+      <LoginStatus />
     </>
   );
 }
