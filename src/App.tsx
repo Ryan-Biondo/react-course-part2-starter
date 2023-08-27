047309c55f247a82e050467903d58ed7dac2577d
Ryan Biondo <biondoryan.dev@gmail.com>
2023-08-27 09:26:03 -0600

    Sharing State using Context

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,21 @@ import TodoList from './react-query/TodoList';
 import Counter from './state-management/Counter';
 import TaskList from './state-management/TaskList';
 import LoginStatus from './state-management/LoginStatus';
+import { useReducer } from 'react';
+import tasksReducer from './state-management/reducers/tasksReducer';
+import NavBar from './routing/NavBar';
+import HomePage from './routing/HomePage';
+import TasksContext from './state-management/contexts/tasksContext';
 
 function App() {
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+
   return (
     <>
-      <LoginStatus />
+      <TasksContext.Provider value={{ tasks, dispatch }}>
+        <NavBar />
+        <HomePage />
+      </TasksContext.Provider>
     </>
   );
 }
