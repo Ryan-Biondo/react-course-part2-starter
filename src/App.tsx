30bdd8d2e826a44ef43cc5910ec0a4db6240b2a2
Ryan Biondo <biondoryan.dev@gmail.com>
2023-08-27 10:04:19 -0600

    working with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import './App.css';
-import TodoForm from './react-query/TodoForm';
-import TodoList from './react-query/TodoList';
-import Counter from './state-management/Counter';
-import TaskList from './state-management/TaskList';
-import LoginStatus from './state-management/LoginStatus';
 import { useReducer } from 'react';
 import tasksReducer from './state-management/reducers/tasksReducer';
-import NavBar from './routing/NavBar';
-import HomePage from './routing/HomePage';
+import NavBar from './state-management/NavBar';
+import HomePage from './state-management/HomePage';
 import TasksContext from './state-management/contexts/tasksContext';
+import authReducer from './state-management/reducers/authReducer';
+import AuthContext from './state-management/contexts/authContext';
 
 function App() {
-  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
+  const [user, authDispatch] = useReducer(authReducer, '');
 
   return (
     <>
-      <TasksContext.Provider value={{ tasks, dispatch }}>
-        <NavBar />
-        <HomePage />
-      </TasksContext.Provider>
+      <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+        <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
+          <NavBar />
+          <HomePage />
+        </TasksContext.Provider>
+      </AuthContext.Provider>
     </>
   );
 }
